58a9059695c702037510e2869202f828706eccb6
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-08 19:57:05 -0700

    rendered PostsView component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // import CSS and components here
+import PostsView from "./features/posts/PostsView";
 
 const App = () => {
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <PostsView />
     </div>
   );
 }
