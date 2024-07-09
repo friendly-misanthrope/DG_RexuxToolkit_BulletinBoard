76ba5dbcce8df9ee16b258d978da2fc1b3a0021b
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-09 00:22:23 -0700

    created component to show authors in post

---
+++ src/features/posts/AuthorView.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+import { selectAllUsers } from "../users/usersSlice";
+
+const AuthorView = () => {
+  return (
+    <div>AuthorView</div>
+  )
+}
+
+export default AuthorView
