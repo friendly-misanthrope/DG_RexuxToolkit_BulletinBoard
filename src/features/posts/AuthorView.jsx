558150d284c086184b6b25b96dd54d31400209dc
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-09 00:35:35 -0700

    selected all users, used Array.find() to find post author

--- src/features/posts/AuthorView.jsx
+++ src/features/posts/AuthorView.jsx
@@ -2,9 +2,10 @@ import { useSelector } from "react-redux";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AuthorView = () => {
-  return (
-    <div>AuthorView</div>
-  )
+  const allUsers = useSelector(selectAllUsers);
+  const author = allUsers.find(user => user.id === userId)
+
+  return <span>by {author? author.name : 'Unknown Author'}</span>
 }
 
-export default AuthorView
+export default AuthorView;
