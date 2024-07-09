cb630ced0694f8de891eabc953e895ae63152733
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-08 21:02:56 -0700

    imported and rendered AddPost form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import CSS and components here
 import PostsView from "./features/posts/PostsView";
+import AddPostView from "./features/posts/AddPostView";
 
 const App = () => {
   return (
     <div className='App'>
+      <AddPostView />
       <PostsView />
     </div>
   );
