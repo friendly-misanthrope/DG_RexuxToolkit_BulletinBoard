a697f1737234e7e0c2695a353df274d8a4b8273d
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-08 19:31:45 -0700

    added initialState w/ 2 posts, created slice, exported slice reducer

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  { 
+    id: '1',
+    title: 'Learning Redux Toolkit',
+    content: "I've heard good things!"
+  },
+  {
+    id: '2',
+    title: 'Slices...',
+    content: "The more I say slice, the more I want a slice of pizza."
+  }
+];
+
+export const postsSlice = createSlice({
+
+});
+
+export default postsSlice.reducer;
