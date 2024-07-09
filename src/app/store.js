9fc2cec53e8e6be4ca8165b007cfeef5c95beac0
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-08 22:11:08 -0700

    Added redux-logger for state & action logging

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from '../features/posts/postsSlice';
+import { logger } from 'redux-logger';
+
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
-  }
+  },
+  middleware: ((getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(logger);
+  })
 });
