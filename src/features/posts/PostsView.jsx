8bdee30c8169bd28aa2781ec28df60cb2b6786fe
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-08 19:42:26 -0700

    setup useSelector, created jsx article content for each post

--- src/features/posts/PostsView.jsx
+++ src/features/posts/PostsView.jsx
@@ -1,6 +1,15 @@
 import { useSelector } from "react-redux";
 
 const PostsView = () => {
+  const posts = useSelector(state => state.posts);
+
+  const postsContent = posts.map(post => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+    </article>
+  ))
+  
   return (
     <div>PostsView</div>
   );
