e7a0d784df9053a08d13ddf676f109230e94ecc3
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-08 22:23:48 -0700

    imported usersReducer and added it to store reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from '../features/posts/postsSlice';
+import usersReducer from '../features/users/usersSlice';
 import { logger } from 'redux-logger';
 
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
+    users: usersReducer
   },
   middleware: ((getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(logger);
