ec210aa0132f4445f94b86ec187b68525a15c1ce
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-09 21:19:37 -0700

    redefined initial state for use with Thunk API calls

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,34 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 import { nanoid } from "@reduxjs/toolkit";
 import { sub } from 'date-fns';
 
-const initialState = [
-  {
-    id: '1',
-    title: 'Learning Redux Toolkit',
-    content: "I've heard good things!",
-    createdAt: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0
-    }
-  },
-  {
-    id: '2',
-    title: 'Slices...',
-    content: "The more I say slice, the more I want a slice of pizza.",
-    createdAt: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0
-    }
-  }
-];
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
 
 export const postsSlice = createSlice({
   name: 'posts',
