b75ed76219451c228c089684da97f89d4fee4838
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-09 18:30:37 -0700

    added createdAt field for initialState posts and in postAdded action payload

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,16 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { nanoid } from "@reduxjs/toolkit";
+import { sub } from 'date-fns';
 
 const initialState = [
   { 
     id: '1',
     title: 'Learning Redux Toolkit',
-    content: "I've heard good things!"
+    content: "I've heard good things!",
+    createdAt: sub(new Date(), { minutes: 10 }).toISOString()
   },
   {
     id: '2',
     title: 'Slices...',
-    content: "The more I say slice, the more I want a slice of pizza."
+    content: "The more I say slice, the more I want a slice of pizza.",
+    createdAt: sub(new Date(), { minutes: 5 }).toISOString()
   }
 ];
 
@@ -28,6 +31,7 @@ export const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            createdAt: new Date().toISOString(),
             userId
           }
         }
