f2cb53705877a4ec2378fbe0aa8b174d75eac298
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-11 23:48:53 -0700

    dispatched fetchUsers immediately upon app loading

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
+import { fetchUsers } from './features/users/usersSlice.js'
 import './index.css'
+
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
